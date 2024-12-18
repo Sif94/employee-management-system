bf7aadebfde6297950e05abeae6a351785475e10
Sifeddine <sifeddinebaouz@gmail.com>
2024-12-18 21:40:59 +0100

    Initialization of the project
    - Add the Spring Boot API project
    - Change the .gitignore file

---
+++ employee-management-system-api/src/test/java/org/baouz/ems_api/EmployeeManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baouz.ems_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
